3e24b408ce2d97153e0de14c82daff0d09849043
SergeyPry <s.prykhozhij@gmail.com>
2018-02-06 12:21:47 -0400

    modified test_that

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,9 +1,6 @@
 library(testthat)
-library(FARfunctions)
 
 test_that("FARfunctions",{
   expect_that(make_filename(2013), equals("accident_2013.csv.bz2"))
-
-  expect_equal(dim(fars_read("accident_2014.csv.bz2"))[1], 30056 )
 }
 )
